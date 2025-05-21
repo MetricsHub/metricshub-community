d2896959c663d0025892a7b19222a61e38942531
Nassim Boutekedjiret <nassim@sentrysoftware.com>
2025-05-21 20:30:36 +0200

    Issue #503: Fix simultaneous reporting of estimated and measured power
    - Fixed unit tests

--- metricshub-hardware/src/test/java/org/metricshub/hardware/util/PowerAndEnergyCollectHelperTest.java
+++ metricshub-hardware/src/test/java/org/metricshub/hardware/util/PowerAndEnergyCollectHelperTest.java
@@ -7,8 +7,10 @@ import java.util.List;
 import java.util.Map;
 import org.junit.jupiter.api.Test;
 import org.metricshub.engine.common.helpers.KnownMonitorType;
+import org.metricshub.engine.configuration.HostConfiguration;
 import org.metricshub.engine.connector.model.Connector;
 import org.metricshub.engine.connector.model.PowerMeasurement;
+import org.metricshub.engine.connector.model.identity.ConnectorIdentity;
 import org.metricshub.engine.telemetry.Monitor;
 import org.metricshub.engine.telemetry.TelemetryManager;
 import org.metricshub.hardware.constants.EnclosureConstants;
@@ -18,25 +20,32 @@ class PowerAndEnergyCollectHelperTest {
 	@Test
 	void testMeasuredConnectorTakesPrecedence() {
 		var connector1 = new Connector();
+		connector1.setConnectorIdentity(ConnectorIdentity.builder().compiledFilename("connector1").build());
 		connector1.setPowerMeasurement(PowerMeasurement.MEASURED);
 		var connector2 = new Connector();
+		connector2.setConnectorIdentity(ConnectorIdentity.builder().compiledFilename("connector2").build());
 		connector2.setPowerMeasurement(PowerMeasurement.ESTIMATED);
 
-		var result = PowerAndEnergyCollectHelper.isPowerMeasured(List.of(connector1, connector2), new TelemetryManager());
+		var telemetryManager = new TelemetryManager();
+		telemetryManager.setHostConfiguration(HostConfiguration.builder().hostname("test").build());
+		var result = PowerAndEnergyCollectHelper.isPowerMeasured(List.of(connector1, connector2), telemetryManager);
 		assertTrue(result, "MEASURED should take precedence over ESTIMATED as consequence Power is measured");
 	}
 
 	@Test
 	void testConditionalWithActivePowerMetric() {
 		var connector1 = new Connector();
+		connector1.setConnectorIdentity(ConnectorIdentity.builder().compiledFilename("connector1").build());
 		connector1.setPowerMeasurement(PowerMeasurement.ESTIMATED);
 		var connector2 = new Connector();
+		connector2.setConnectorIdentity(ConnectorIdentity.builder().compiledFilename("connector2").build());
 		connector2.setPowerMeasurement(PowerMeasurement.CONDITIONAL);
 
 		var monitor = new Monitor();
 		monitor.getConditionalCollection().put(EnclosureConstants.HW_ENCLOSURE_POWER, "120");
 
 		var telemetryManager = new TelemetryManager();
+		telemetryManager.setHostConfiguration(HostConfiguration.builder().hostname("test").build());
 		telemetryManager.setMonitors(Map.of(KnownMonitorType.ENCLOSURE.getKey(), Map.of("e1", monitor)));
 
 		var result = PowerAndEnergyCollectHelper.isPowerMeasured(List.of(connector1, connector2), telemetryManager);
@@ -46,6 +55,7 @@ class PowerAndEnergyCollectHelperTest {
 	@Test
 	void testConditionalButPowerMetricDeactivated() {
 		var connector = new Connector();
+		connector.setConnectorIdentity(ConnectorIdentity.builder().compiledFilename("connector").build());
 		connector.setPowerMeasurement(PowerMeasurement.CONDITIONAL);
 
 		var monitor = new Monitor();
@@ -53,6 +63,7 @@ class PowerAndEnergyCollectHelperTest {
 		// "" implies deactivation in Monitor.isMetricDeactivated logic
 
 		var telemetryManager = new TelemetryManager();
+		telemetryManager.setHostConfiguration(HostConfiguration.builder().hostname("test").build());
 		telemetryManager.setMonitors(Map.of(KnownMonitorType.ENCLOSURE.getKey(), Map.of("e1", monitor)));
 
 		var result = PowerAndEnergyCollectHelper.isPowerMeasured(List.of(connector), telemetryManager);
@@ -66,26 +77,33 @@ class PowerAndEnergyCollectHelperTest {
 	void testOnlyEstimatedNoMonitors() {
 		var connector = new Connector();
 		connector.setPowerMeasurement(PowerMeasurement.ESTIMATED);
+		connector.setConnectorIdentity(ConnectorIdentity.builder().compiledFilename("connector").build());
 
-		var result = PowerAndEnergyCollectHelper.isPowerMeasured(List.of(connector), new TelemetryManager());
+		var telemetryManager = new TelemetryManager();
+		telemetryManager.setHostConfiguration(HostConfiguration.builder().hostname("test").build());
+		var result = PowerAndEnergyCollectHelper.isPowerMeasured(List.of(connector), telemetryManager);
 		assertFalse(result, "Only ESTIMATED and no monitors should result in false as consequence Power is not measured");
 	}
 
 	@Test
 	void testNullConnectorList() {
-		var result = PowerAndEnergyCollectHelper.isPowerMeasured(null, new TelemetryManager());
+		var telemetryManager = new TelemetryManager();
+		telemetryManager.setHostConfiguration(HostConfiguration.builder().hostname("test").build());
+		var result = PowerAndEnergyCollectHelper.isPowerMeasured(null, telemetryManager);
 		assertFalse(result, "Null connector list should result in false as consequence Power is not measured");
 	}
 
 	@Test
 	void testConditionalWithActiveEnergyMetric() {
 		var connector = new Connector();
+		connector.setConnectorIdentity(ConnectorIdentity.builder().compiledFilename("connector").build());
 		connector.setPowerMeasurement(PowerMeasurement.CONDITIONAL);
 
 		var monitor = new Monitor();
 		monitor.getConditionalCollection().put(EnclosureConstants.HW_ENCLOSURE_ENERGY, "15319239489");
 
 		var telemetryManager = new TelemetryManager();
+		telemetryManager.setHostConfiguration(HostConfiguration.builder().hostname("test").build());
 		telemetryManager.setMonitors(Map.of(KnownMonitorType.ENCLOSURE.getKey(), Map.of("e1", monitor)));
 
 		var result = PowerAndEnergyCollectHelper.isPowerMeasured(List.of(connector), telemetryManager);
@@ -95,12 +113,14 @@ class PowerAndEnergyCollectHelperTest {
 	@Test
 	void testConditionalWithNonActiveEnergyMetric() {
 		var connector = new Connector();
+		connector.setConnectorIdentity(ConnectorIdentity.builder().compiledFilename("connector").build());
 		connector.setPowerMeasurement(PowerMeasurement.CONDITIONAL);
 
 		var monitor = new Monitor();
 		monitor.getConditionalCollection().put("some.metric", "15319239489");
 
 		var telemetryManager = new TelemetryManager();
+		telemetryManager.setHostConfiguration(HostConfiguration.builder().hostname("test").build());
 		telemetryManager.setMonitors(Map.of(KnownMonitorType.ENCLOSURE.getKey(), Map.of("e1", monitor)));
 
 		var result = PowerAndEnergyCollectHelper.isPowerMeasured(List.of(connector), telemetryManager);
