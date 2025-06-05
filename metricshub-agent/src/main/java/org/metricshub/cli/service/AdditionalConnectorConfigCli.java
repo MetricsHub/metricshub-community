18e60eaf42ec3732dda2c18a98f2fb033902c11d
Elyes Cherfa <elyes@sentrysoftware.com>
2025-06-05 15:06:07 +0200

    Issue #357: MetricsHub CLI: Added the ability to specify connector variables
    * Added the ability to specify connector variables
    * Added unit tests for additionalConnectorsCli.
    * Functionally tested on the CLI.

---
+++ metricshub-agent/src/main/java/org/metricshub/cli/service/AdditionalConnectorConfigCli.java
@@ -0,0 +1,49 @@
+package org.metricshub.cli.service;
+
+/*-
+ * ╱╲╱╲╱╲╱╲╱╲╱╲╱╲╱╲╱╲╱╲╱╲╱╲╱╲╱╲╱╲╱╲╱╲╱╲╱╲╱╲
+ * MetricsHub Agent
+ * ჻჻჻჻჻჻
+ * Copyright 2023 - 2025 MetricsHub
+ * ჻჻჻჻჻჻
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * ╲╱╲╱╲╱╲╱╲╱╲╱╲╱╲╱╲╱╲╱╲╱╲╱╲╱╲╱╲╱╲╱╲╱╲╱╲╱╲╱
+ */
+
+import java.util.Map;
+import lombok.Data;
+import org.metricshub.engine.configuration.AdditionalConnector;
+import picocli.CommandLine.Option;
+
+@Data
+public class AdditionalConnectorConfigCli {
+
+	@Option(names = { "--additionalConnector", "--additional-connector" }, required = true)
+	String connectorId;
+
+	@Option(names = "--uses")
+	String uses;
+
+	@Option(names = { "-F", "--variable" }, description = "Connector variable in key=value format")
+	Map<String, String> variables;
+
+	AdditionalConnector toAdditionalConnector() {
+		return AdditionalConnector
+			.builder()
+			.force(true)
+			.uses(uses != null ? uses : connectorId)
+			.variables(variables)
+			.build();
+	}
+}
