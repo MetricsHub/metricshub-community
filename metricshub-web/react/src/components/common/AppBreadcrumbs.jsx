5e80cbfa82e12972fc6840d9a8f7ec94978d596d
lancelot <lancelot@sentrysoftware.com>
2026-01-07 10:51:47 +0100

    feat: add breadcrumb support for resource connectors in Explorer routes

--- metricshub-web/react/src/components/common/AppBreadcrumbs.jsx
+++ metricshub-web/react/src/components/common/AppBreadcrumbs.jsx
@@ -37,6 +37,30 @@ const EXPLORER_ROUTES = [
 		},
 	},
 	{
+		pattern: "/explorer/resource-groups/:group/resources/:resource/connectors/:connectorId",
+		getBreadcrumbs: (params) => {
+			const group = decodeURIComponent(params.group);
+			const resource = decodeURIComponent(params.resource);
+			const connectorId = decodeURIComponent(params.connectorId);
+			return [
+				{ label: group, to: paths.explorerResourceGroup(group), iconType: "resource-group" },
+				{ label: resource, to: paths.explorerResource(group, resource), iconType: "resource" },
+				{ label: connectorId, to: null },
+			];
+		},
+	},
+	{
+		pattern: "/explorer/resources/:resource/connectors/:connectorId",
+		getBreadcrumbs: (params) => {
+			const resource = decodeURIComponent(params.resource);
+			const connectorId = decodeURIComponent(params.connectorId);
+			return [
+				{ label: resource, to: paths.explorerResource(null, resource), iconType: "resource" },
+				{ label: connectorId, to: null },
+			];
+		},
+	},
+	{
 		pattern: "/explorer/resource-groups/:group/resources/:resource",
 		getBreadcrumbs: (params) => {
 			const group = decodeURIComponent(params.group);
