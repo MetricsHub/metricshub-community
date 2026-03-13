00152a4a7426a5809cbe3a9f440ea78e39ac6f6c
Nassim Boutekedjiret <nassim@sentrysoftware.com>
2026-03-13 09:29:32 +0100

    Issue #1148: Intermittent collection failures
    - Fixed unit tests

--- metricshub-agent/src/test/java/org/metricshub/agent/service/scheduling/SelfSchedulingTest.java
+++ metricshub-agent/src/test/java/org/metricshub/agent/service/scheduling/SelfSchedulingTest.java
@@ -96,13 +96,13 @@ class SelfSchedulingTest {
 		assertNotNull(resourceAttributes.get(AGENT_INFO_CC_VERSION_NUMBER_ATTRIBUTE_KEY));
 		assertNotNull(resourceAttributes.get(COMPANY_ATTRIBUTE_KEY));
 
-		// Verify the metrics
+		// Verify the metrics (1 agent info + 6 thread pool stats = 7 total)
 		final List<ScopeMetrics> scopeMetricsList = resourceMetrics.getScopeMetricsList();
 		assertEquals(1, scopeMetricsList.size());
 		final ScopeMetrics scopeMetrics = scopeMetricsList.get(0);
-		assertEquals(1, scopeMetrics.getMetricsCount());
+		assertEquals(7, scopeMetrics.getMetricsCount());
 		final List<Metric> metrics = scopeMetrics.getMetricsList();
-		assertEquals(1, metrics.size());
+		assertEquals(7, metrics.size());
 		final Metric metric = metrics.get(0);
 		assertEquals(AgentInfo.METRICS_HUB_AGENT_METRIC_NAME, metric.getName());
 		assertEquals(SelfScheduling.METRICS_HUB_AGENT_INFORMATION, metric.getDescription());
