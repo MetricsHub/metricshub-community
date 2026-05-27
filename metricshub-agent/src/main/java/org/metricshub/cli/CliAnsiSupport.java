1ee6c716878534bb96c40254bd028d582928644e
Nassim Boutekedjiret <nassim@sentrysoftware.com>
2026-05-27 16:42:56 +0200

    Issue #1219: Migrate MetricsHub project from Java 17 to Java 25
    Fixed header in CliAnsiSupport.java to reflect the new copyright year

--- metricshub-agent/src/main/java/org/metricshub/cli/CliAnsiSupport.java
+++ metricshub-agent/src/main/java/org/metricshub/cli/CliAnsiSupport.java
@@ -4,7 +4,7 @@ package org.metricshub.cli;
  * ╱╲╱╲╱╲╱╲╱╲╱╲╱╲╱╲╱╲╱╲╱╲╱╲╱╲╱╲╱╲╱╲╱╲╱╲╱╲╱╲
  * MetricsHub Agent
  * ჻჻჻჻჻჻
- * Copyright 2023 - 2025 MetricsHub
+ * Copyright 2023 - 2026 MetricsHub
  * ჻჻჻჻჻჻
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
